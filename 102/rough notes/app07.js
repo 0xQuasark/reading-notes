96e2c3201ef5167d5333db5238f95df876a28fae
Quasark <q@quasark.xyz>
2023-06-29 10:20:15 -0700

    thu morning changes

---
+++ 102/rough notes/app07.js
@@ -0,0 +1,11 @@
+function sayName(name) {
+  document.write('Hello ' + name + '<p>')
+}
+
+function whatTimeIsIt(militaryTime) {
+  if (militaryTime < 12) {
+    document.write('It is morning');
+  } else {
+    document.write('It is the afternoon/evening');
+  }
+}
